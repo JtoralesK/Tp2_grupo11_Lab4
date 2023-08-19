1d7864c9d3902681465fe01d7ff40f0f80c7201b
javier torales <jtorales2016@gmail.com>
2023-08-19 19:06:12 -0300

    se agrego pauta 3 y main

---
+++ src/ejercicio3/Polideportivo.java
@@ -0,0 +1,31 @@
+package ejercicio3;
+
+public class Polideportivo extends Edificio implements InstalacionDeportiva {
+	private String nombre;
+	private int tipoDeInstalacion=0;
+	@Override
+	public String toString() {
+		return "Polideportivo [nombre=" + nombre + ", getTipoDeInstalacion()=" + getTipoDeInstalacion()
+				+ ", getSuperficeEdificio()=" + getSuperficeEdificio() + "]";
+	}
+
+	public Polideportivo(double superficeEdificio,String nombre) {
+		super(superficeEdificio);
+		this.nombre= nombre;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getTipoDeInstalacion() {
+		// TODO Auto-generated method stub
+		 this.tipoDeInstalacion;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+}
---
+++ src/ejercicio3/mainEjercicio3.java
@@ -0,0 +1,22 @@
+package ejercicio3;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class mainEjercicio3 {
+	public static void main (String[] args) {
+		 /*ArrayList<Edificio> edificios = new ArrayList<Edificio>();
+		 edificios.add(new Polideportivo(1000.2,"Polideportivo A")) ;
+		 edificios.add(new Polideportivo(2100,"Polideportivo B")) ;
+		 edificios.add(new Polideportivo(505.2,"Polideportivo C")) ;
+
+		 edificios.add(new EdificioOficinas(505.2,20)) ;
+		 edificios.add(new EdificioOficinas(505.2,10)) ;
+		Iterator<Edificio> iterador = edificios.iterator();
+			while (iterador.hasNext()) {
+				Edificio edificio = iterador.next();
+				System.out.println(edificio.toString());
+			}*/
+	}
+
+}
