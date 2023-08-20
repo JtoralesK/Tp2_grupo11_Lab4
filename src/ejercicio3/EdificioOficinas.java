2daedb316a46053f184a2f2b759881264c6f0d3a
Coqui2 <francomarcolongo98@gmail.com>
2023-08-20 14:48:12 -0300

    se agrega clase de oficinas

---
+++ src/ejercicio3/EdificioOficinas.java
@@ -0,0 +1,25 @@
+package ejercicio3;
+
+public class EdificioOficinas extends Edificio{
+	private int oficinas;
+	
+	public EdificioOficinas(double superficie, int oficinas) {
+		super(superficie);
+		this.oficinas=oficinas;
+	}
+	
+	public int getOficinas() {
+		return oficinas;
+	}
+
+	public void setOficinas(int oficinas) {
+		this.oficinas = oficinas;
+	}
+
+	@Override
+	public String toString() {
+		return "EdificioOficinas cantidad de oficinas: " + oficinas+ ", superficie: "+this.getSuperficie();
+	}
+	
+	
+}
